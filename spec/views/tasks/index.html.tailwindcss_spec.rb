44305e3218d99583602dc0587c260fbac859350f
Nattapat Teeranuntachai <bossnattapat238@gmail.com>
2025-05-29 10:10:44 +0700

    create Task controller

---
+++ spec/views/tasks/index.html.tailwindcss_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(
+        title: "Title",
+        status: "Status"
+      ),
+      Task.create!(
+        title: "Title",
+        status: "Status"
+      )
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Status".to_s), count: 2
+  end
+end
