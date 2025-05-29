cf3d42eea2475c837c93eeca350b5f73c9461f30
Nattapat Teeranuntachai <bossnattapat238@gmail.com>
2025-05-29 10:39:51 +0700

    feat: add completed boolean column to tasks and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_29_031021) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_033528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,5 +20,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_29_031021) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
   end
 end
